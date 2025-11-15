77886715fec731cfd8c72eae675cbcb51a0d5d4d
Sheikh Tamim <tamimsheikh142@gmail.com>
2025-11-15 13:41:50 +0600

    Upload leave.js

---
+++ others/leave.js
@@ -0,0 +1,96 @@
+const { getTime, drive } = global.utils;
+
+module.exports = {
+	config: {
+		name: "leave",
+		version: "1.4",
+		author: "ST | Sheikh Tamim",
+		category: "events"
+	},
+
+	langs: {
+		vi: {
+			session1: "sáng",
+			session2: "trưa",
+			session3: "chiều",
+			session4: "tối",
+			leaveType1: "tự rời",
+			leaveType2: "bị kick",
+			defaultLeaveMessage: "{userName} đã {type} khỏi nhóm"
+		},
+		en: {
+			session1: "morning",
+			session2: "noon",
+			session3: "afternoon",
+			session4: "evening",
+			leaveType1: "left",
+			leaveType2: "was kicked from",
+			defaultLeaveMessage: "{userName} {type} the group"
+		}
+	},
+
+	onStart: async ({ threadsData, message, event, api, usersData, getLang }) => {
+		if (event.logMessageType == "log:unsubscribe")
+			return async function () {
+				const { threadID } = event;
+				const threadData = await threadsData.get(threadID);
+				if (!threadData.settings.sendLeaveMessage)
+					return;
+				const { leftParticipantFbId } = event.logMessageData;
+				if (leftParticipantFbId == api.getCurrentUserID())
+					return;
+				const hours = getTime("HH");
+
+				const threadName = threadData.threadName;
+				let userName;
+				
+				try {
+					const userInfo = await api.getUserInfo(leftParticipantFbId);
+					userName = userInfo[leftParticipantFbId]?.name;
+				} catch (err) {
+					console.error("Error getting user info:", err);
+				}
+				
+				if (!userName) {
+					userName = await usersData.getName(leftParticipantFbId);
+				}
+				
+				if (!userName || userName === leftParticipantFbId) {
+					userName = "Unknown User";
+				}
+
+				let { leaveMessage = getLang("defaultLeaveMessage") } = threadData.data;
+
+				leaveMessage = leaveMessage
+					.replace(/\{userName\}/g, userName)
+					.replace(/\{userNameTag\}/g, userName)
+					.replace(/\{type\}/g, leftParticipantFbId == event.author ? getLang("leaveType1") : getLang("leaveType2"))
+					.replace(/\{threadName\}|\{boxName\}/g, threadName)
+					.replace(/\{time\}/g, hours)
+					.replace(/\{session\}/g, hours <= 10 ?
+						getLang("session1") :
+						hours <= 12 ?
+							getLang("session2") :
+							hours <= 18 ?
+								getLang("session3") :
+								getLang("session4")
+					);
+
+				const form = {
+					body: leaveMessage
+				};
+
+				if (threadData.data.leaveAttachment) {
+					const files = threadData.data.leaveAttachment;
+					const attachments = files.reduce((acc, file) => {
+						acc.push(drive.getFile(file, "stream"));
+						return acc;
+					}, []);
+					form.attachment = (await Promise.allSettled(attachments))
+						.filter(({ status }) => status == "fulfilled")
+						.map(({ value }) => value);
+				}
+				message.send(form);
+			};
+	}
+};
